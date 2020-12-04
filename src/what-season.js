2ced0062a9b2ba895603c01cf089dfc810d36517
Elena <lensnig@gmail.com>
2020-12-04 15:00:00 +0400

    feat: add solution for task transform-array

--- src/what-season.js
+++ src/what-season.js
@@ -2,8 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
-  if (Object.prototype.toString.call(date) != '[object Date]') 
-    throw new Error('');
+  if (Object.prototype.toString.call(date) != '[object Date]') {
+    throw new Error('THROWN');
+  }
   let season = '';
   let month = date.getMonth();
   if ((month < 2) || (month == 11)) 
