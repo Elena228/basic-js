9a2d5b2472b8fdf10599f42c111a5e8f9eb1ade1
Elena <lensnig@gmail.com>
2020-12-17 10:13:10 +0400

    add solution for task vegenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addition = '';
+  let result = '';
+  if (options.additionRepeatTimes) {
+    options.additionSeparator =  (options.additionSeparator) || '|';
+    const separateArr = new Array(options.additionRepeatTimes).fill(String(options.addition));
+    addition = separateArr.join(options.additionSeparator);
+  }
+  else if (options.addition) {
+    addition = String(options.addition);
+  }
+
+  if (!options.repeatTimes) options.repeatTimes = 1; 
+  options.separator = (options.separator) || '+';
+  const repeatArr = new Array(options.repeatTimes).fill((String(str) + addition));
+  result = repeatArr.join(options.separator);
+  
+  return result;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.typeOfMachine = type;
+  }
+
+  encrypt(message, key) {
+    if ((message === undefined) || (key === undefined)) {
+    throw new Error('');
+    }
+    let result = [];
+    let strArr = message.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('');
+    let i = 0;
+    let arg;
+    strArr.forEach(element => {
+      if (/[A-Z]/.test(element)) {
+        arg = ((element.codePointAt(0)-65) + (keyArr[i].codePointAt(0)-65)) % 26;
+        i = (i < key.length - 1)? (i + 1): 0;
+        result.push(String.fromCodePoint(arg + 65));
+      }
+      else {
+        result.push(element);
+      }
+    }); 
+    return this.typeOfMachine? result.join(''): result.reverse().join('');
+  }   
+  
+  
+  decrypt(message, key) {
+    if ((message === undefined) || (key === undefined)) {
+      throw new Error('');
+      } 
+    let result = [];
+    let strArr = message.split('');
+    let keyArr = key.toUpperCase().split('');
+    let i = 0;
+    let arg;
+    strArr.forEach(element => {
+      if (/[A-Z]/.test(element)) {
+        arg = ((element.codePointAt(0)-65) - (keyArr[i].codePointAt(0)-65) + 26) % 26;
+        i = (i < key.length - 1)? (i + 1): 0;
+        result.push(String.fromCodePoint(arg + 65));
+      }
+      else {
+        result.push(element);
+      }
+    }); 
+    return this.typeOfMachine? result.join(''): result.reverse().join('');
   }
 }
 
